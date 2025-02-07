e2c82ba0f67adc786b526e4f7956db3c4c33e354
qkmeng <qkmeng@whu.edu.cn>
2025-02-07 17:52:04 +0800

    in processing,params catch and analys

---
+++ params_monitor.py
@@ -0,0 +1,34 @@
+import torch
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset
+from TransformerClassifier_model import TransformerClassifier
+import tiktoken
+import pandas as pd
+
+from SpamDataset_class import  SpamDataset
+
+def get_model_params(model):
+    total_params = 0
+    param_details = []
+
+    for name, param in model.named_parameters():
+        param_count = param.numel()
+        total_params += param_count
+        param_details.append(f"{name}: {param_count} parameters")
+
+    return total_params, param_details
+
+
+
+vocab_size = 50257  # The tokenizer size
+model = TransformerClassifier(vocab_size=vocab_size)
+model.load_state_dict(torch.load("models/my_trans.pth"))
+model.eval()
+
+
+
+total_params, param_details=get_model_params(model)
+print(total_params)
+print(param_details)
